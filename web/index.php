45d585daf2bf32629f9fda307c847328ff10542d
Jonathan Carlson <Car99020@byui.edu>
2019-04-23 15:32:52 -0500

    Uploading assignment page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-US">
 
-  phpinfo();
+    <head>
+        <meta charset="utf-8">
+        <meta name="homepage" content="width=device-width, initial-scale=1">
+        <title>CS313 Jonathan Carlson</title>
+        <meta name = "description" content="CS313 Assignments for Jonathan Carlson.">
+        <link rel="stylesheet" href="styles/normalize.css">
+        <link id="styles" rel="stylesheet" href="styles/main.css">
+    </head>
 
-?>
+    <body>
+        <div class="align">
+            <header>
+                <img src="images/jon.jpg" width="200" alt="Profile picture">
+                <h1> Jonathan Carlson</h1>
+            </header>
+        </div>
+        <div class="align">
+            <main>
+                <h2>CS313 Web Engineering II</h2>
+                <h3>Assignments:</h3>
+                <ul>
+                    <a href=""><li>About Me</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                    <a href=""><li>(comming soon!)</li></a>
+                </ul>
+                <div id="fixed">
+                    <button type="button" onclick="document.getElementById('styles').href='styles/main.css'">Style 1</button>
+                    <button type="button" onclick="document.getElementById('styles').href='styles/main2.css'">Style 2</button>
+                </div>
+            </main>  
+            <footer>  
+                <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
+                
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ web/modules/footer.php
@@ -0,0 +1,7 @@
+<p>&copy; 2019 &#9670; Jonathan Carlson &#9670; CS313 - BYU-Idaho &#9670; Huntsville Alabama &#9670; Car99020@BYUI.edu</p>
+
+<p>    
+<?php
+echo date("l, ") . date("M d, Y");
+?>
+</p>
