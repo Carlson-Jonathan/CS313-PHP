5ac736db6952484c20a57cf0ae705c2d20e700de
Jonathan Carlson <Car99020@byui.edu>
2019-05-20 20:58:15 -0500

    uploading teach05

--- web/teamactivities/teach05/teach05.php
+++ web/teamactivities/teach05/teach05.php
@@ -1,50 +1,5 @@
 <!DOCTYPE html>
 <html lang="en-US">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="Teach05" content="width=device-width, initial-scale=1">
-        <title>CS313 Teach05 Team1</title>
-        <meta name = "description" content="CS313 Teach05 Team 1">
-        <link rel="stylesheet" href="normalize.css">
-        <link id="styles" rel="stylesheet" href="teach05.css">
-    </head>
-    
-    <header>
-    </header>
-    
-    <body>
-     
-        <?php
-            include($_SERVER['DOCUMENT_ROOT'].'/scripts/postgresqlsetup.php');
-            echo '<p>Scripture Resrouces</p>';
-
-          foreach (db as $row) {
-                print "<span class='bold'>" . $row['book'] . " " . $row['chapter'] .
-                ":" . $row['verse'] . "</span>";
-            }
-
-        ?>
-
-
-        
-    </body>
-        
-    <footer>="Teach05" content="width=device-width, initial-scale=1">
-        <title>CS313 Teach05 Team1</title>
-        <meta name = "description" content="CS313 Teach05 Team 1">
-        <link rel="stylesheet" href="normalize.css">
-        <link id="styles" rel="stylesheet" href="teach05.css">
-    </head> 
-    
-    <header>
-
-
-    </header>
-    
-    <body>
-     
-        <?php
-    </footer>
     
 </html>
